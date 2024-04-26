0abfe0759d24d75a21b1485ae91df92f3eed7b52
Philip <4144601+philipbrembeck@users.noreply.github.com>
2024-04-27 00:30:16 +0200

    fix: Peta Controller

--- src/peta/peta.controller.ts
+++ src/peta/peta.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Param, Res, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
-import * as path from 'path';
-import { ApiResponse, ApiTags, ApiProperty, ApiExcludeEndpoint } from "@nestjs/swagger";
+import { Controller, Get, Param, Res, HttpStatus } from "@nestjs/common";
+import { Response } from "express";
+import * as path from "path";
+import {
+  ApiResponse,
+  ApiTags,
+  ApiProperty,
+  ApiExcludeEndpoint,
+} from "@nestjs/swagger";
 
 export class petaType {
   @ApiProperty({
@@ -11,12 +16,15 @@ export class petaType {
   type: string = "crueltyfree";
 }
 
-@Controller('v0/peta')
+@Controller("v0/peta")
 export class PetaController {
-  
-  @Get(':type')
+  @Get(":type")
   @ApiTags("Peta")
-  @ApiProperty({ description: "The type of the peta list", example: "crueltyfree", type: petaType})
+  @ApiProperty({
+    description: "The type of the peta list",
+    example: "crueltyfree",
+    type: petaType,
+  })
   @ApiResponse({
     status: 200,
     description: "Request returned a positive result.",
@@ -33,35 +41,38 @@ export class PetaController {
     status: 500,
     description: "Internal Server Error.",
   })
-  getPetaByType(@Param('type') type: string, @Res() res: Response) {
+  getPetaByType(@Param("type") type: string, @Res() res: Response) {
     if (!type || type.length === 0) {
       return res.status(HttpStatus.BAD_REQUEST).json({
-        status: '400',
-        code: 'Bad request',
-        message: 'Missing argument type'
+        status: "400",
+        code: "Bad request",
+        message: "Missing argument type",
       });
     }
 
-    if (type === 'crueltyfree') {
-      return res.status(HttpStatus.OK).sendFile(path.join(__dirname, '../peta_cruelty_free.json'), {
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8'
-        }
-      });
+    if (type === "crueltyfree") {
+      return res
+        .status(HttpStatus.OK)
+        .sendFile(path.join(__dirname, "../../peta_cruelty_free.json"), {
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        });
     }
 
-    if (type === 'veganapproved') {
+    if (type === "veganapproved") {
       return res.status(HttpStatus.NOT_IMPLEMENTED).json({
-        status: '501',
-        code: 'Not implemented',
-        message: 'veganapproved was removed from the API'
+        status: "501",
+        code: "Not implemented",
+        message: "veganapproved was removed from the API",
       });
     }
 
     return res.status(HttpStatus.NOT_FOUND).json({
-      status: '404',
-      code: 'Type not found',
-      message: 'The given type was not found in the API. Please check the documentation for available types. Also check for typos.'
+      status: "404",
+      code: "Type not found",
+      message:
+        "The given type was not found in the API. Please check the documentation for available types. Also check for typos.",
     });
   }
 
@@ -69,9 +80,9 @@ export class PetaController {
   @ApiExcludeEndpoint()
   getPetaWithoutType(@Res() res: Response) {
     return res.status(HttpStatus.BAD_REQUEST).json({
-      status: '400',
-      code: 'Bad request',
-      message: 'Missing argument type: /v0/peta/:type'
+      status: "400",
+      code: "Bad request",
+      message: "Missing argument type: /v0/peta/:type",
     });
   }
 }
