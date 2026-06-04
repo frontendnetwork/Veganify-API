36be9ca028e383c105742065172f88e046863b53
Philip <4144601+philipbrembeck@users.noreply.github.com>
2026-06-04 08:59:52 +0200

    chore: update deps and pin actions

--- src/ingredients/shared/pipes/parse-boolean.pipe.ts
+++ src/ingredients/shared/pipes/parse-boolean.pipe.ts
@@ -9,6 +9,6 @@ export class ParseBooleanPipe implements PipeTransform {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: string | undefined, metadata: ArgumentMetadata): boolean {
     // If value is not provided or not 'false', return true (default behavior)
-    return !value || value.toLowerCase() !== "false";
+    return value?.toLowerCase() !== "false";
   }
 }
